97215266d9c7310c226d4d1bd17ecacfc001c2f3
Rafael Batista <maranata_rafa@hotmail.com>
2024-12-04 22:44:08 +0000

    FEAT: [recover-password] ensure return correct application code and message from usecase when fail

--- internal/domain/auth/port/adapter/recover_password_adapter.go
+++ internal/domain/auth/port/adapter/recover_password_adapter.go
@@ -3,6 +3,7 @@ package authadapter
 import (
 	"encoding/json"
 	"getfund-api-v2/internal/domain/auth/usecase/recoverpassword"
+	"getfund-api-v2/internal/shared/resultapp"
 	"net/http"
 )
 
@@ -10,10 +11,13 @@ func (h *authAdapter) RecoverPassword(w http.ResponseWriter, r *http.Request) (i
 	var input recoverpassword.Input
 
 	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
-		return nil, http.StatusBadRequest, err
+		return nil, resultapp.BAD_REQUEST, err
 	}
 
-	h.recoverPassword.Execute(&input)
+	_, err := h.recoverPassword.Execute(&input)
+	if err != nil {
+		return nil, err.Code, err.Message
+	}
 
 	return nil, 0, nil
 }
