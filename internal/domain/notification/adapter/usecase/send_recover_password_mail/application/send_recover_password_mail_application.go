e1a019b27586f93d8daeebac157d5a1e7625f8c9
Rafael Batista <maranata_rafa@hotmail.com>
2024-12-22 17:25:10 +0000

    FEAT: [send-recover-password-mail] ensure calls cache with correct parameter

--- internal/domain/notification/adapter/usecase/send_recover_password_mail/application/send_recover_password_mail_application.go
+++ internal/domain/notification/adapter/usecase/send_recover_password_mail/application/send_recover_password_mail_application.go
@@ -3,15 +3,19 @@ package send_recover_password_mail_application
 import (
 	"getfund-api-v2/internal/domain/notification/adapter/usecase/send_recover_password_mail"
 	"getfund-api-v2/internal/shared/result_app"
+	"getfund-api-v2/internal/shared/service/cache_service"
 )
 
 type sendRecoverPasswordMailApplication struct {
-	//cahce
+	cacheService cache_service.Cache
 	//email service
+	//settings
 }
 
-func New() send_recover_password_mail.UseCase {
-	return &sendRecoverPasswordMailApplication{}
+func New(cacheService cache_service.Cache) send_recover_password_mail.UseCase {
+	return &sendRecoverPasswordMailApplication{
+		cacheService: cacheService,
+	}
 }
 
 func (uc *sendRecoverPasswordMailApplication) Execute(input *send_recover_password_mail.Input) (*send_recover_password_mail.Output, *result_app.ApplicationError) {
@@ -19,6 +23,8 @@ func (uc *sendRecoverPasswordMailApplication) Execute(input *send_recover_passwo
 	if input.IsInvalid() {
 		return nil, result_app.New(result_app.BAD_REQUEST_CODE, input.GetErrors())
 	}
+
+	uc.cacheService.Get(input.KeyCache)
 
 	return nil, nil
 }
