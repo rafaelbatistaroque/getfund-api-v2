c9e94c3f729d31ab8683ac3fd8e24251183a5688
Rafael Batista <maranata_rafa@hotmail.com>
2024-12-30 23:19:40 +0000

    TEST: [mail-service] ensure code coverage return erro if Content parameter null or empty

--- test/Internal/domain/notification/adapter/domain_service/mail_service/mail_service_test.go
+++ test/Internal/domain/notification/adapter/domain_service/mail_service/mail_service_test.go
@@ -1,7 +1,6 @@
 package mail_service_test
 
 import (
-	"getfund-api-v2/internal/domain/notification/adapter/domain_service/mail_service"
 	"getfund-api-v2/pkg/verify"
 	fixture "getfund-api-v2/test/internal/domain/notification/adapter/domain_service/mail_service/mail_service_fixture"
 	"testing"
@@ -20,11 +19,22 @@ func Test_GivenSendMail_WhenInvalidToParam_ThenEnsureReturnError(t *testing.T) {
 
 func Test_GivenSendMail_WhenInvalidSubjectParam_ThenEnsureReturnError(t *testing.T) {
 	// Arrange
-	sut := mail_service.New()
+	sut := fixture.NewSUT()
 
 	// Act
 	err := sut.SendMail(fixture.GetFakeEmailParams(fixture.WithoutSubject()))
 
 	// Assert
 	verify.Should(t, err.Error()).Be("parameter Subject cannot be null or empty")
+}
+
+func Test_GivenSendMail_WhenInvalidContentParam_ThenEnsureReturnError(t *testing.T) {
+	// Arrange
+	sut := fixture.NewSUT()
+
+	// Act
+	err := sut.SendMail(fixture.GetFakeEmailParams(fixture.WithoutContent()))
+
+	// Assert
+	verify.Should(t, err.Error()).Be("parameter Content cannot be null or empty")
 }
