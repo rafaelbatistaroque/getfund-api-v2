2334f41d5c4cbcd0b516e6cb50ddaaeeeb8db2a2
Rafael Batista <maranata_rafa@hotmail.com>
2024-12-31 00:10:52 +0000

    FEAT: [gomail config] ensure set From key as correct parameter

---
+++ pkg/mail/mail.go
@@ -0,0 +1,16 @@
+package mail
+
+import (
+	"getfund-api-v2/internal/shared/contract/settings"
+	logger "getfund-api-v2/pkg/log"
+
+	"gopkg.in/gomail.v2"
+)
+
+func New(settings settings.ApplicationSettings) (*gomail.Message, *gomail.Dialer) {
+	mail := gomail.NewMessage()
+	logger.New("SMTP mail config")
+	mail.SetHeader("From", settings.GetSMTPFrom())
+
+	return mail, nil
+}
