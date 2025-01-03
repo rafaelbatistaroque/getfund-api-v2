4944e172c950469409d04a94e3bc83fdf62aa114
Rafael Batista <maranata_rafa@hotmail.com>
2025-01-03 23:47:34 +0000

    TEST: [reset-password] ensure code coverage on return appropriate error if recovery code null or empty

---
+++ test/Internal/domain/auth/adapter/usecase/reset_password/reset_password_fixture/reset_password_fixture.go
@@ -0,0 +1,70 @@
+package reset_password_fixture
+
+import (
+	"getfund-api-v2/internal/domain/auth/adapter/usecase/reset_password"
+	sut "getfund-api-v2/internal/domain/auth/adapter/usecase/reset_password/application"
+
+	"github.com/google/uuid"
+)
+
+type ResetPasswordFixture struct {
+}
+
+func NewSut() (reset_password.UseCase, *ResetPasswordFixture) {
+
+	return sut.New(), nil
+}
+
+type Option func(*reset_password.Input)
+
+func GetInput(options ...Option) *reset_password.Input {
+	input := &reset_password.Input{RecoveryCode: uuid.NewString() + uuid.NewString(), Password: "strongPassword123"}
+
+	for _, opt := range options {
+		opt(input)
+	}
+
+	return input
+}
+
+func WithRecoveryCodeEmpty() Option {
+	return func(params *reset_password.Input) {
+		params.RecoveryCode = ""
+	}
+}
+
+func WithRecoveryCodeInvalid() Option {
+	return func(params *reset_password.Input) {
+		params.RecoveryCode = "invalid_recovery_code"
+	}
+}
+
+func WithPasswordEmpty() Option {
+	return func(params *reset_password.Input) {
+		params.Password = ""
+	}
+}
+
+func WithInvalidLengthPassword() Option {
+	return func(params *reset_password.Input) {
+		params.Password = "-"
+	}
+}
+
+func WithInvalidUpperPassword() Option {
+	return func(params *reset_password.Input) {
+		params.Password = "abcdef12"
+	}
+}
+
+func WithInvalidLowerPassword() Option {
+	return func(params *reset_password.Input) {
+		params.Password = "ABCDEF12"
+	}
+}
+
+func WithInvalidDigitPassword() Option {
+	return func(params *reset_password.Input) {
+		params.Password = "ABCDefgh"
+	}
+}
---
+++ test/Internal/domain/auth/adapter/usecase/reset_password/reset_password_test.go
@@ -0,0 +1,25 @@
+package reset_password_test
+
+import (
+	"fmt"
+	reset_password_application "getfund-api-v2/internal/domain/auth/adapter/usecase/reset_password/application"
+	"getfund-api-v2/internal/shared/result_app"
+	fixture "getfund-api-v2/test/internal/domain/auth/adapter/usecase/reset_password/reset_password_fixture"
+	"testing"
+
+	"github.com/rafaelbatistaroque/validation"
+	"github.com/rafaelbatistaroque/verify"
+)
+
+func Test_GivenExecute_WhenInputRecoveryCodeEmpty_ThenEnsureReturnErrorFromValidate(t *testing.T) {
+	// Arrange
+	invalidInput := fixture.GetInput(fixture.WithRecoveryCodeEmpty())
+	sut := reset_password_application.New()
+
+	// Act
+	_, err := sut.Execute(invalidInput)
+
+	// Assert
+	verify.Should(t, err.Code).Be(result_app.BAD_REQUEST_CODE)
+	verify.Should(t, err.Message).Be(fmt.Errorf(validation.Err_PARAMETER_NOT_EMPTY.Error(), "RecoveryCode"))
+}
