20a38c7aeda2b66bd386015618912134ab9b439f
Rafael Batista <maranata_rafa@hotmail.com>
2025-01-06 19:37:10 +0000

    FEAT: [reset-password] ensure call cache get with corret parameter

--- internal/domain/auth/adapter/usecase/reset_password/application/reset_password_application.go
+++ internal/domain/auth/adapter/usecase/reset_password/application/reset_password_application.go
@@ -3,13 +3,21 @@ package reset_password_application
 import (
 	"getfund-api-v2/internal/domain/auth/adapter/usecase/reset_password"
 	"getfund-api-v2/internal/shared/result_app"
+	"getfund-api-v2/internal/shared/service/cache_service"
+)
+
+var (
+	_KEY_CACHE_PREFIX = "recovery_password_"
 )
 
 type resetPasswordApplication struct {
+	cacheService cache_service.Cache
 }
 
-func New() *resetPasswordApplication {
-	return &resetPasswordApplication{}
+func New(cacheService cache_service.Cache) *resetPasswordApplication {
+	return &resetPasswordApplication{
+		cacheService: cacheService,
+	}
 }
 
 func (r *resetPasswordApplication) Execute(input *reset_password.Input) (*reset_password.Output, *result_app.ApplicationError) {
@@ -17,5 +25,8 @@ func (r *resetPasswordApplication) Execute(input *reset_password.Input) (*reset_
 	if validatable.IsInvalid() {
 		return nil, result_app.New(result_app.BAD_REQUEST_CODE, validatable.GetErrors())
 	}
+
+	r.cacheService.Get(_KEY_CACHE_PREFIX + input.RecoveryCode)
+
 	return nil, nil
 }
