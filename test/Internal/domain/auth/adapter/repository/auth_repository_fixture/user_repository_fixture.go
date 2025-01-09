ff7f57e15b124b78f0df52e005ac176eb08c28f4
Rafael Batista <maranata_rafa@hotmail.com>
2025-01-09 23:47:54 +0000

    TEST: [auth-repository] ensure code coverage on return null if update operation success

--- test/Internal/domain/auth/adapter/repository/auth_repository_fixture/user_repository_fixture.go
+++ test/Internal/domain/auth/adapter/repository/auth_repository_fixture/user_repository_fixture.go
@@ -1,7 +1,7 @@
-package user_repository_fixture
+package auth_repository_fixture
 
 import (
-	authauthRepository "getfund-api-v2/internal/domain/auth/adapter/auth_repository"
+	"getfund-api-v2/internal/domain/auth/adapter/auth_repository"
 	auth_contract "getfund-api-v2/internal/domain/auth/core/contract"
 	"getfund-api-v2/test/helper/db_fixture"
 	"math/rand"
@@ -13,28 +13,29 @@ import (
 func NewSUT() (auth_contract.AuthRepository, *gorm.DB) {
 	db := db_fixture.NewMemoryDB(&FakeUser{})
 
-	return authauthRepository.New(db), db
+	return auth_repository.New(db), db
 }
 
 type FakeUser struct {
 	ID       string `json:"id"`
 	Username string `json:"username"`
 	IsActive int    `json:"is_active"`
+	Password string `json:"password"`
 }
 
 func (FakeUser) TableName() string {
 	return "user"
 }
 
-func AddUser(db *gorm.DB, quantity int, diff bool) {
+func AddRandomUser(db *gorm.DB, quantity int, diff bool) {
 	if diff {
 		for range quantity {
-			db.Create(FakeUser{ID: uuid.NewString(), Username: uuid.NewString(), IsActive: rand.Intn(2)})
+			db.Create(FakeUser{ID: uuid.NewString(), Username: uuid.NewString(), IsActive: rand.Intn(2), Password: uuid.NewString()})
 		}
 	} else {
 		user := uuid.NewString()
 		for range quantity {
-			db.Create(FakeUser{ID: user, Username: user, IsActive: rand.Intn(2)})
+			db.Create(FakeUser{ID: user, Username: user, IsActive: rand.Intn(2), Password: uuid.NewString()})
 		}
 	}
 
