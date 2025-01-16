11d4211d4caaf8946eb8cdc6127d99b96fe5c268
Rafael Batista <maranata_rafa@hotmail.com>
2025-01-16 09:35:22 +0000

    FEAT: [create-user-gateway] ensure call usecase after success decode body

--- internal/domain/user/adapter/gateway/create_user_gateway.go
+++ internal/domain/user/adapter/gateway/create_user_gateway.go
@@ -10,10 +10,11 @@ import (
 func (u *userGateway) CreateUser(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
 	var input create_user.Input
 
-	err := json.NewDecoder(r.Body).Decode(&input)
-	if err != nil {
+	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
 		return nil, result_app.BAD_REQUEST_CODE, err
 	}
+
+	u.createUser.Execute(&input)
 
 	return nil, 0, nil
 }
