24547b18a9453f858495a0f4b9b9a925b91bc6d3
Rafael Batista <maranata_rafa@hotmail.com>
2025-01-17 10:56:19 +0000

    TEST: [create-user-application] ensure test coverage on fail-first validation with firstName required rules

---
+++ test/Internal/domain/user/core/usecase/create_user/create_user_fixture/create_user_fixture.go
@@ -0,0 +1,32 @@
+package create_user_fixture
+
+import (
+	"getfund-api-v2/internal/domain/user/core/usercase/create_user"
+	create_user_application "getfund-api-v2/internal/domain/user/core/usercase/create_user/application"
+)
+
+type CreateUserFixture struct {
+}
+
+func NewSut() (create_user.UseCase, *CreateUserFixture) {
+
+	return create_user_application.New(), &CreateUserFixture{}
+}
+
+type Option func(*create_user.Input)
+
+func GetInput(options ...Option) *create_user.Input {
+	input := &create_user.Input{FirstName: "fakeFirstNameValid", Password: "strongPassword123"}
+
+	for _, opt := range options {
+		opt(input)
+	}
+
+	return input
+}
+
+func WithFirstNameEmpty() Option {
+	return func(params *create_user.Input) {
+		params.FirstName = ""
+	}
+}
---
+++ test/Internal/domain/user/core/usecase/create_user/create_user_test.go
@@ -0,0 +1,24 @@
+package create_user_test
+
+import (
+	"fmt"
+	"getfund-api-v2/internal/shared/result_app"
+	fixture "getfund-api-v2/test/internal/domain/user/core/usecase/create_user/create_user_fixture"
+	"testing"
+
+	"github.com/rafaelbatistaroque/validation"
+	"github.com/rafaelbatistaroque/verify"
+)
+
+func Test_GivenCreateUserExecute_WhenInputFirstNameEmpty_ThenEnsureReturnError(t *testing.T) {
+	// Arrange
+	sut, _ := fixture.NewSut()
+	invalidInput := fixture.GetInput(fixture.WithFirstNameEmpty())
+
+	// Act
+	_, err := sut.Execute(invalidInput)
+
+	// Assert
+	verify.Should(t, err.Code).Be(result_app.BAD_REQUEST_CODE)
+	verify.Should(t, err.Message.Error()).Contain(fmt.Sprintf(validation.Err_PARAMETER_NOT_EMPTY.Error(), "FirstName"))
+}
