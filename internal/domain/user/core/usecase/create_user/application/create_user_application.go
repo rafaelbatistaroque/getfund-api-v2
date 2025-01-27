cd409048301ea2239eea7e5d08ad4e30d0b15178
Rafael Batista <maranata_rafa@hotmail.com>
2025-01-27 12:41:54 +0000

    FEAT: [create-user-application][#3] ensure return duplicated entry error if GetUserByUsername found

--- internal/domain/user/core/usecase/create_user/application/create_user_application.go
+++ internal/domain/user/core/usecase/create_user/application/create_user_application.go
@@ -1,6 +1,7 @@
 package create_user_application
 
 import (
+	"errors"
 	user_contract "getfund-api-v2/internal/domain/user/core/contract"
 	"getfund-api-v2/internal/domain/user/core/usecase/create_user"
 	"getfund-api-v2/internal/shared/result_app"
@@ -22,9 +23,13 @@ func (c *createUserApplication) Execute(input *create_user.Input) (*create_user.
 		return nil, result_app.New(result_app.BAD_REQUEST_CODE, validated.GetErrors())
 	}
 
-	_, err := c.repository.GetUserByUsername(input.Email)
+	userDuplicated, err := c.repository.GetUserByUsername(input.Email)
 	if err != nil {
 		return nil, result_app.New(result_app.NOT_FOUND_CODE, err)
+	}
+
+	if userDuplicated != nil {
+		return nil, result_app.New(result_app.DUPLICATED_ENTRY_CODE, errors.New("User already exists"))
 	}
 
 	return nil, nil
