61dc40196fb33815b5128433162aef31e73a6f0f
Rafael Batista <maranata_rafa@hotmail.com>
2025-02-04 21:34:51 +0000

    FEAT: [activate-user-usecase][#9] ensure return call cache get with correct parameter

--- internal/domain/user/core/usecase/activate_user/application/activate_user_application.go
+++ internal/domain/user/core/usecase/activate_user/application/activate_user_application.go
@@ -3,13 +3,21 @@ package activate_user_application
 import (
 	"getfund-api-v2/internal/domain/user/core/usecase/activate_user"
 	"getfund-api-v2/internal/shared/result_app"
+	"getfund-api-v2/internal/shared/service/cache_service"
+)
+
+const (
+	_KEY_USER_ACTIVATION_PREFIX = "user_activation_"
 )
 
 type activateUserApplication struct {
+	cache cache_service.Cache
 }
 
-func New() activate_user.UseCase {
-	return &activateUserApplication{}
+func New(cache cache_service.Cache) activate_user.UseCase {
+	return &activateUserApplication{
+		cache: cache,
+	}
 }
 
 func (a *activateUserApplication) Execute(input *activate_user.Input) (*activate_user.Output, *result_app.ApplicationError) {
@@ -17,5 +25,9 @@ func (a *activateUserApplication) Execute(input *activate_user.Input) (*activate
 	if validatable.IsInvalid() {
 		return nil, result_app.New(result_app.UNAUTHORIZED_CODE, validatable.GetErrors())
 	}
+
+	keyCache := _KEY_USER_ACTIVATION_PREFIX + input.ActivationCode
+	a.cache.Get(keyCache)
+
 	return nil, nil
 }
