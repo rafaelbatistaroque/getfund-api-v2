51723d909ff3419364c0b6cfa382abedbdd78fda
Rafael Batista <maranata_rafa@hotmail.com>
2025-02-04 01:31:51 +0000

    FEAT: [activate-user-gateway][#34] ensure return success if usecase success

--- internal/domain/user/adapter/activate_user_gateway/activate_user_gateway.go
+++ internal/domain/user/adapter/activate_user_gateway/activate_user_gateway.go
@@ -35,10 +35,10 @@ func (u *activeUserGateway) ActivateUser(w http.ResponseWriter, r *http.Request)
 
 	input := activate_user.Input{ActivationCode: activationCode}
 
-	_, err := u.activateUser.Execute(&input)
+	output, err := u.activateUser.Execute(&input)
 	if err != nil {
 		return nil, err.Code, err.Message
 	}
 
-	return nil, 0, nil
+	return output, result_app.SUCCESS_CODE, nil
 }
