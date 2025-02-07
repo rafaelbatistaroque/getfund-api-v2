f27d03b8f312e1b4ad35cd7f0837477f5530dec9
Rafael Batista <maranata_rafa@hotmail.com>
2025-02-07 18:40:20 +0000

    REFACT: remove unnecessary test

---
+++ test/internal/domain/user/core/usecase/activate_user/activate_user_test.go
@@ -0,0 +1,117 @@
+package activate_user_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"getfund-api-v2/internal/domain/user/core/user_dto"
+	"getfund-api-v2/internal/shared/result_app"
+	fixture "getfund-api-v2/test/internal/domain/user/core/usecase/activate_user/activate_user_fixture"
+	"testing"
+
+	"github.com/rafaelbatistaroque/validation"
+	"github.com/rafaelbatistaroque/verify"
+)
+
+func Test_GivenExecute_WhenActivationCodeEmpty_ThenEnsureReturnError(t *testing.T) {
+	// Arrange
+	sut, _ := fixture.NewSut()
+	inputWithActivationCodeEmpty := fixture.GetInput(fixture.WithEmptyActivationCode())
+
+	// Act
+	_, err := sut.Execute(inputWithActivationCodeEmpty)
+
+	// Assert
+	verify.Should(t, err.Code).Be(result_app.UNAUTHORIZED_CODE)
+	verify.Should(t, err.Message.Error()).Contain(fmt.Sprintf(validation.Err_PARAMETER_NOT_EMPTY.Error(), "ActivationCode"))
+}
+
+func Test_GivenExecute_WhenActivationCodeInvalid_ThenEnsureReturnError(t *testing.T) {
+	// Arrange
+	sut, _ := fixture.NewSut()
+	inputWithActivationCodeInvalid := fixture.GetInput(fixture.WithInvalidActivationCodeLength())
+
+	// Act
+	_, err := sut.Execute(inputWithActivationCodeInvalid)
+
+	// Assert
+	verify.Should(t, err.Code).Be(result_app.UNAUTHORIZED_CODE)
+	verify.Should(t, err.Message.Error()).Contain(fmt.Sprintf(validation.Err_PARAMETER_SHOULD_HAVE_EXACTLY_CHARACTER.Error(), "ActivationCode", 20))
+}
+
+func Test_GivenExecute_WhenInputValid_ThenEnsureCallCacheGetWithCorrectParameter(t *testing.T) {
+	// Arrange
+	sut, spies := fixture.NewSut()
+	validInput := fixture.GetInput()
+	expectedParam := "user_activation_" + validInput.ActivationCode
+
+	// Act
+	sut.Execute(validInput)
+
+	// Assert
+	verify.Should(t, spies.CacheSpy.Params["Get:key"]).Be(expectedParam)
+}
+
+func Test_GivenExecute_WhenCacheGetInvoked_ThenEnsureCallsOnce(t *testing.T) {
+	// Arrange
+	sut, spies := fixture.NewSut()
+
+	// Act
+	sut.Execute(fixture.GetInput())
+
+	// Assert
+	verify.Should(t, spies.CacheSpy.CallsCount["Get"]).Be(1)
+}
+
+func Test_GivenExecute_WhenCacheGetError_ThenEnsureReturnNotFoundErrorWIthApropriateMessage(t *testing.T) {
+	// Arrange
+	sut, spies := fixture.NewSut()
+	spies.CacheSpy.DefineCacheGetError()
+
+	// Act
+	_, err := sut.Execute(fixture.GetInput())
+
+	// Assert
+	verify.Should(t, err.Code).Be(result_app.NOT_FOUND_CODE)
+	verify.Should(t, err.Message.Error()).Be("activation code not found")
+}
+
+func Test_GivenExecute_WhenUnmarshalError_ThenEnsureReturnAppropriateError(t *testing.T) {
+	// Arrange
+	sut, spies := fixture.NewSut()
+	spies.CacheSpy.DefineCacheGetSuccess(`{error-data}`)
+
+	// Act
+	_, err := sut.Execute(fixture.GetInput())
+
+	// Assert
+	verify.Should(t, err.Code).Be(result_app.SERVER_ERROR_CODE)
+	verify.Should(t, err.Message.Error()).Be("error on get user data")
+}
+
+func Test_GivenExecute_WhenUnmarshalSuccess_ThenEnsureCallGetUserByUsernameWithCorrectParameter(t *testing.T) {
+	// Arrange
+	sut, spies := fixture.NewSut()
+	spies.CacheSpy.DefineCacheGetSuccess(`{"email": "fake-valid@mail.com"}`)
+	var expectedParam = user_dto.ActivationUserDto{}
+	json.Unmarshal([]byte(spies.CacheSpy.SuccessResult["Get"].(string)), &expectedParam)
+
+	// Act
+	sut.Execute(fixture.GetInput())
+
+	// Assert
+	verify.Should(t, spies.RepoSpy.Params["GetUserByUsername:username"]).Be(expectedParam.Email)
+}
+
+func Test_GivenExecute_WhenGetUserByUsernameError_ThenEnsureReturnNotFoundError(t *testing.T) {
+	// Arrange
+	sut, spies := fixture.NewSut()
+	spies.CacheSpy.DefineCacheGetSuccess("")
+	spies.RepoSpy.DefineGetUserByUsernameError()
+
+	// Act
+	_, err := sut.Execute(fixture.GetInput())
+
+	// Assert
+	verify.Should(t, err.Code).Be(result_app.SERVER_ERROR_CODE)
+	verify.Should(t, err.Message).Be(spies.RepoSpy.ErrorResult["GetUserByUsername"])
+}
