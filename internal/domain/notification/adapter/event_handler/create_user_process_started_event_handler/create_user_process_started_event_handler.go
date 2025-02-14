82a90e6844129d98f72bea5c6d80d75cd8d5a3c4
Rafael Batista <maranata_rafa@hotmail.com>
2025-02-14 18:34:57 +0000

    FEAT: [send-activation-account-mail] ensure log and return when cache get error

--- internal/domain/notification/adapter/event_handler/create_user_process_started_event_handler/create_user_process_started_event_handler.go
+++ internal/domain/notification/adapter/event_handler/create_user_process_started_event_handler/create_user_process_started_event_handler.go
@@ -2,20 +2,23 @@ package create_user_process_started_event_handler
 
 import (
 	"encoding/json"
+	"getfund-api-v2/internal/domain/notification/core/usecase/send_activation_account_mail"
 	"getfund-api-v2/internal/shared/service/cache_service"
 	"getfund-api-v2/pkg/bus"
 	logger "getfund-api-v2/pkg/log"
 )
 
 type createUserProcessStartedEventHandler struct {
-	logger logger.Logger
-	cache  cache_service.Cache
+	logger                    logger.Logger
+	cache                     cache_service.Cache
+	sendActivationAccountMail send_activation_account_mail.UseCase
 }
 
-func New(cache cache_service.Cache) bus.Handler {
+func New(cache cache_service.Cache, sendActivationAccountMail send_activation_account_mail.UseCase) bus.Handler {
 	return &createUserProcessStartedEventHandler{
-		logger: *logger.New("createUserProcessStartedEventHandler"),
-		cache:  cache,
+		logger:                    *logger.New("createUserProcessStartedEventHandler"),
+		cache:                     cache,
+		sendActivationAccountMail: sendActivationAccountMail,
 	}
 }
 
@@ -31,5 +34,11 @@ func (h *createUserProcessStartedEventHandler) Handle(event bus.Event) {
 		return
 	}
 
-	h.cache.Get(payload.ActivationCode)
+	_, err := h.cache.Get(payload.ActivationCode)
+	if err != nil {
+		h.logger.Error("IsOk: False | get cache failed")
+		return
+	}
+
+	h.sendActivationAccountMail.Execute(nil)
 }
--- internal/domain/notification/core/usecase/send_activation_account_mail/send_activation_account_mail_usecase.go
+++ internal/domain/notification/core/usecase/send_activation_account_mail/send_activation_account_mail_usecase.go
@@ -1,7 +1,9 @@
 package send_activation_account_mail
 
+import "getfund-api-v2/internal/shared/result_app"
+
 type UseCase = sendActivationAccountMail
 
 type sendActivationAccountMail interface {
-	Execute(input *Input) (*Output, error)
+	Execute(input *Input) (*Output, *result_app.ApplicationError)
 }
