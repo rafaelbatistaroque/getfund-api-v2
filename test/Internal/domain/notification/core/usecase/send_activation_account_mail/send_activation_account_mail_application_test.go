25c576e51dfbd273cc8846abf513308dd3b7f978
Rafael Batista <maranata_rafa@hotmail.com>
2025-02-15 19:20:20 +0000

    TEST: [send-activation-account-mail] ensure test coverage on return unprocessable error when input email is null or empty

--- test/Internal/domain/notification/core/usecase/send_activation_account_mail/send_activation_account_mail_application_test.go
+++ test/Internal/domain/notification/core/usecase/send_activation_account_mail/send_activation_account_mail_application_test.go
@@ -21,3 +21,15 @@ func Test_GivenExecute_WhenFirstNameEmpty_ThenEnsureReturnUnprocessableError(t *
 	verify.Should(t, err.Code).Be(result_app.UNPROCESSABLE_CONTENT_CODE)
 	verify.Should(t, err.Message.Error()).Contain(fmt.Sprintf(validation.Err_PARAMETER_NOT_EMPTY.Error(), "FirstName"))
 }
+
+func Test_GivenExecute_WhenEmailEmpty_ThenEnsureReturnUnprocessableError(t *testing.T) {
+	// Arrange
+	sut, _ := fixture.NewSUT()
+
+	// Act
+	_, err := sut.Execute(fixture.GetInvalidInput())
+
+	// Assert
+	verify.Should(t, err.Code).Be(result_app.UNPROCESSABLE_CONTENT_CODE)
+	verify.Should(t, err.Message.Error()).Contain(fmt.Sprintf(validation.Err_PARAMETER_NOT_EMPTY.Error(), "Email"))
+}
