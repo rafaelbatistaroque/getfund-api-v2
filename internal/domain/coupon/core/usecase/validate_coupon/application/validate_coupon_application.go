5d23289f092c1f93138c590f13c82b855c2b11e5
Rafael Batista <maranata_rafa@hotmail.com>
2025-02-21 23:02:25 +0000

    FEAT: [validation-coupon] ensure return apropriate erro when EmitWithPayloadAndResponse response with null prizedraw

--- internal/domain/coupon/core/usecase/validate_coupon/application/validate_coupon_application.go
+++ internal/domain/coupon/core/usecase/validate_coupon/application/validate_coupon_application.go
@@ -79,6 +79,10 @@ func (v *validateCouponApplication) Execute(input *validate_coupon.Input) (*vali
 			return nil, result_app.New(result_app.UNAVAILABLE_CODE, errors.New("error on get coupon data [product invalid]"))
 		}
 
+		if couponData.PrizeDraw == nil {
+			return nil, result_app.New(result_app.UNAVAILABLE_CODE, errors.New("error on get coupon data [prizedraw invalid]"))
+		}
+
 		return nil, nil
 	case <-time.After(time.Duration(v.settings.GetTimeoutResponseEvent()) * time.Second):
 		return nil, result_app.New(result_app.UNAVAILABLE_CODE, errors.New("error on get coupon data [timeout]"))
