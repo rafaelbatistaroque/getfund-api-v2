ebfd5b98481e8072c02c267cc6affcfd4e8de920
Rafael Batista <maranata_rafa@hotmail.com>
2025-02-21 15:49:08 +0000

    FEAT: [validation-coupon] ensure return apropriate error when coupon startAt greather than now

--- internal/domain/coupon/core/usecase/validate_coupon/application/validate_coupon_application.go
+++ internal/domain/coupon/core/usecase/validate_coupon/application/validate_coupon_application.go
@@ -1,9 +1,15 @@
 package validate_coupon_application
 
 import (
+	"errors"
 	coupon_contract "getfund-api-v2/internal/domain/coupon/core/contract"
 	"getfund-api-v2/internal/domain/coupon/core/usecase/validate_coupon"
 	"getfund-api-v2/internal/shared/result_app"
+	"time"
+)
+
+var (
+	_NOW = uint64(time.Now().Unix())
 )
 
 type validateCouponApplication struct {
@@ -22,9 +28,15 @@ func (v *validateCouponApplication) Execute(input *validate_coupon.Input) (*vali
 		return nil, result_app.New(result_app.UNPROCESSABLE_CONTENT_CODE, validatable.GetErrors())
 	}
 
-	_, err := v.repository.GetCouponByCode(input.CouponCode)
+	couponFound, err := v.repository.GetCouponByCode(input.CouponCode)
 	if err != nil {
 		return nil, result_app.New(result_app.NOT_FOUND_CODE, err)
 	}
+
+	if couponFound.StartAt > _NOW {
+		return nil, result_app.New(result_app.UNAVAILABLE_CODE, errors.New("coupon validity has not start yet"))
+	}
+
+	//"coupon validity has not start yet"
 	return nil, nil
 }
