9a991d0d48992cec3729f9d0bd6de43804a11e21
Rafael Batista <maranata_rafa@hotmail.com>
2025-02-21 23:08:36 +0000

    REFACT: create constants for messages

--- internal/domain/coupon/core/usecase/validate_coupon/application/validate_coupon_application.go
+++ internal/domain/coupon/core/usecase/validate_coupon/application/validate_coupon_application.go
@@ -15,9 +15,18 @@ import (
 	"time"
 )
 
-var (
-	_NOW = time.Now().Unix()
+const (
+	_RESPONSE_EMPTY     = "error on get coupon data [response empty]"
+	_RESPONSE_INVALID   = "error on get coupon data [response invalid]"
+	_PRODUCT_INVALID    = "error on get coupon data [product invalid]"
+	_PRIZE_DRAW_INVALID = "error on get coupon data [prizedraw invalid]"
+	_TIME_OUT           = "error on get coupon data [timeout]"
+	_COUPON_EXPIRED     = "coupon expired"
+	_HAS_NOT_START      = "coupon validity has not start yet"
+	_FOUND_NULL         = "error on get coupon data [found null]"
 )
+
+var _NOW = time.Now().Unix()
 
 type validateCouponApplication struct {
 	repository coupon_contract.Repository
@@ -45,15 +54,15 @@ func (v *validateCouponApplication) Execute(input *validate_coupon.Input) (*vali
 	}
 
 	if couponFound == nil {
-		return nil, result_app.New(result_app.SERVER_ERROR_CODE, errors.New("error on get coupon data [found null]"))
+		return nil, result_app.New(result_app.SERVER_ERROR_CODE, errors.New(_FOUND_NULL))
 	}
 
 	if couponFound.StartAt > _NOW {
-		return nil, result_app.New(result_app.UNAVAILABLE_CODE, errors.New("coupon validity has not start yet"))
+		return nil, result_app.New(result_app.UNAVAILABLE_CODE, errors.New(_HAS_NOT_START))
 	}
 
 	if couponFound.EndAt != nil && *couponFound.EndAt < _NOW {
-		return nil, result_app.New(result_app.UNAVAILABLE_CODE, errors.New("coupon expired"))
+		return nil, result_app.New(result_app.UNAVAILABLE_CODE, errors.New(_COUPON_EXPIRED))
 	}
 
 	responseChannel := make(chan []byte, 2)
@@ -67,24 +76,24 @@ func (v *validateCouponApplication) Execute(input *validate_coupon.Input) (*vali
 	select {
 	case response := <-responseChannel:
 		if bytes.Equal(response, app_constant.EMPTYB) {
-			return nil, result_app.New(result_app.UNAVAILABLE_CODE, errors.New("error on get coupon data [response empty]"))
+			return nil, result_app.New(result_app.UNAVAILABLE_CODE, errors.New(_RESPONSE_EMPTY))
 		}
 
 		var couponData = &coupon_dto.CouponValidationData{}
 		if err := json.Unmarshal(response, &couponData); err != nil {
-			return nil, result_app.New(result_app.UNAVAILABLE_CODE, errors.New("error on get coupon data [response invalid]"))
+			return nil, result_app.New(result_app.UNAVAILABLE_CODE, errors.New(_RESPONSE_INVALID))
 		}
 
 		if couponData.Product == nil {
-			return nil, result_app.New(result_app.UNAVAILABLE_CODE, errors.New("error on get coupon data [product invalid]"))
+			return nil, result_app.New(result_app.UNAVAILABLE_CODE, errors.New(_PRODUCT_INVALID))
 		}
 
 		if couponData.PrizeDraw == nil {
-			return nil, result_app.New(result_app.UNAVAILABLE_CODE, errors.New("error on get coupon data [prizedraw invalid]"))
+			return nil, result_app.New(result_app.UNAVAILABLE_CODE, errors.New(_PRIZE_DRAW_INVALID))
 		}
 
 		return nil, nil
 	case <-time.After(time.Duration(v.settings.GetTimeoutResponseEvent()) * time.Second):
-		return nil, result_app.New(result_app.UNAVAILABLE_CODE, errors.New("error on get coupon data [timeout]"))
+		return nil, result_app.New(result_app.UNAVAILABLE_CODE, errors.New(_TIME_OUT))
 	}
 }
