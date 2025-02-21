06866b3f411cf44309a4d4803e05538acecb1dfa
Rafael Batista <maranata_rafa@hotmail.com>
2025-02-21 14:15:09 +0000

    FEAT: [validation-coupon] ensure call GetCouponByCode with correct parameter

--- internal/domain/coupon/core/usecase/validate_coupon/application/validate_coupon_application.go
+++ internal/domain/coupon/core/usecase/validate_coupon/application/validate_coupon_application.go
@@ -1,15 +1,19 @@
 package validate_coupon_application
 
 import (
+	coupon_contract "getfund-api-v2/internal/domain/coupon/core/contract"
 	"getfund-api-v2/internal/domain/coupon/core/usecase/validate_coupon"
 	"getfund-api-v2/internal/shared/result_app"
 )
 
 type validateCouponApplication struct {
+	repository coupon_contract.Repository
 }
 
-func New() validate_coupon.UseCase {
-	return &validateCouponApplication{}
+func New(repository coupon_contract.Repository) validate_coupon.UseCase {
+	return &validateCouponApplication{
+		repository: repository,
+	}
 }
 
 func (v *validateCouponApplication) Execute(input *validate_coupon.Input) (*validate_coupon.Output, *result_app.ApplicationError) {
@@ -17,5 +21,7 @@ func (v *validateCouponApplication) Execute(input *validate_coupon.Input) (*vali
 	if validatable.IsInvalid() {
 		return nil, result_app.New(result_app.UNPROCESSABLE_CONTENT_CODE, validatable.GetErrors())
 	}
+
+	v.repository.GetCouponByCode(input.CouponCode)
 	return nil, nil
 }
