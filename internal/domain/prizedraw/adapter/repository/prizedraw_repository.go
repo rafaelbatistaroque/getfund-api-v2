7013639dfd70458651bf85d5b3f7abe2a5a0c6dd
Rafael Batista <maranata_rafa@hotmail.com>
2025-03-01 18:39:18 +0000

    FEAT: [prizedraw-repository] ensure return erro when GetCouponByCode error

---
+++ internal/domain/prizedraw/adapter/repository/prizedraw_repository.go
@@ -0,0 +1,35 @@
+package prizedraw_repository
+
+import (
+	prizedraw_contract "getfund-api-v2/internal/domain/prizedraw/core/contract"
+	"getfund-api-v2/internal/domain/prizedraw/core/dto/prizedraw_dto"
+	"getfund-api-v2/pkg/db/schema"
+
+	"gorm.io/gorm"
+)
+
+type prizedrawRepository struct {
+	db *gorm.DB
+}
+
+func New(db *gorm.DB) prizedraw_contract.Repository {
+	return &prizedrawRepository{db: db}
+}
+
+func (p *prizedrawRepository) GetCouponByCode(couponCode string) (*prizedraw_dto.CouponDto, error) {
+	var coupon = &schema.Coupon{}
+
+	result := p.db.
+		Select("").
+		Find(coupon)
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return nil, nil
+}
+
+func (p *prizedrawRepository) GetPrizeDrawById(id int) (*prizedraw_dto.PrizeDrawDto, error) {
+	return nil, nil
+}
