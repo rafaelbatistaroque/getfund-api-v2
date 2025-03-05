271ba6d6326bef7affdbb165b223bbac9d5c8259
Rafael Batista <maranata_rafa@hotmail.com>
2025-03-05 23:33:06 +0000

    TEST: [apply-coupon] ensure call EmitWithPayloadAndResponse with correct parameter

--- test/internal/domain/prizedraw/core/usecase/apply_prizedraw_coupon/apply_prizedraw_coupon_test.go
+++ test/internal/domain/prizedraw/core/usecase/apply_prizedraw_coupon/apply_prizedraw_coupon_test.go
@@ -2,6 +2,8 @@ package apply_prizedraw_coupon_test
 
 import (
 	"fmt"
+	"getfund-api-v2/internal/domain/prizedraw/core/dto/prizedraw_payload"
+	"getfund-api-v2/internal/domain/prizedraw/core/usecase/apply_prizedraw_coupon"
 	"getfund-api-v2/internal/shared/result_app"
 	fixture "getfund-api-v2/test/internal/domain/prizedraw/core/usecase/apply_prizedraw_coupon/apply_prizedraw_coupon_fixture"
 	"testing"
@@ -60,4 +62,26 @@ func Test_GivenExecute_WhenUserIdZero_ThenEnsureReturnError(t *testing.T) {
 	// Assert
 	verify.Should(t, err.Code).Be(result_app.UNPROCESSABLE_CONTENT_CODE)
 	verify.Should(t, err.Message.Error()).Contain(fmt.Sprintf(validation.Err_PARAMETER_SHOULD_BE_GREATHER_THAN_ZERO.Error(), "UserId"))
+}
+
+func Test_GivenExecute_WhenValidInput_ThenEnsureCallPublishWithPayloadWithCorrectParameter(t *testing.T) {
+	// Arrange
+	sut, spies := fixture.NewSut()
+	validInput := fixture.GetInput()
+	expectedPayload := &prizedraw_payload.ApplyPrizeDrawCouponStartedPayload{
+		UserId:       validInput.UserId,
+		ProductId:    validInput.ProductId,
+		PrizeDrawId:  validInput.PrizeDrawId,
+		CouponId:     validInput.CouponId,
+		ItemQuantity: 1,
+	}
+
+	// Act
+	sut.Execute(validInput)
+
+	// Assert
+	_, isChannelType := spies.BusSpy.Params["EmitWithPayloadAndResponse:responseChannel"][0].(chan []byte)
+	verify.Should(t, isChannelType).BeTrue()
+	verify.Should(t, spies.BusSpy.Params["EmitWithPayloadAndResponse:event"][0]).Be(&apply_prizedraw_coupon.ApplyPrizeDrawCouponStartedEvent{})
+	verify.Should(t, spies.BusSpy.Params["EmitWithPayloadAndResponse:payload"][0]).Be(expectedPayload)
 }
