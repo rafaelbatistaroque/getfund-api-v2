f4b97ca5df2dc262faf1e8e0e95880d45a567895
Rafael Batista <maranata_rafa@hotmail.com>
2025-05-14 10:27:31 +0100

    TEST: [product-repository] ensure return product when found

--- test/internal/domain/product/adapter/repository/product_repository_test.go
+++ test/internal/domain/product/adapter/repository/product_repository_test.go
@@ -1,6 +1,7 @@
 package product_repository_test
 
 import (
+	"getfund-api-v2/pkg/db/schema"
 	fixture "getfund-api-v2/test/internal/domain/product/adapter/repository/product_repository_fixture"
 	"testing"
 
@@ -29,4 +30,20 @@ func Test_GivenGetProductById_WhenNotFound_ThenEnsureReturnError(t *testing.T) {
 
 	// Assert
 	verify.Should(t, err.Error()).Be("product not found")
+}
+
+func Test_GivenGetProductById_WhenProductFound_ThenEnsureReturnSuccess(t *testing.T) {
+	// Arrange
+	sut, db := fixture.NewSUT()
+	productType := &schema.Product{IsActive: true}
+	db.Create(productType)
+
+	// Act
+	productFound, err := sut.GetProductById(1)
+
+	// Assert
+	verify.Should(t, err).Nil()
+	verify.Should(t, productFound).NotNil()
+	verify.Should(t, productFound.Id).Be(1)
+	verify.Should(t, productFound.IsActive).BeTrue()
 }
