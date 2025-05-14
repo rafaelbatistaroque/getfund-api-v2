0c48a35004da06a43c6da42a7b37983a014a7f6c
Rafael Batista <maranata_rafa@hotmail.com>
2025-05-14 10:03:48 +0100

    FEAT: [product-repository] ensure return erro when query fail

---
+++ internal/domain/product/adapter/repository/product_repository.go
@@ -0,0 +1,32 @@
+package product_repository
+
+import (
+	product_contract "getfund-api-v2/internal/domain/product/core/contract"
+	"getfund-api-v2/internal/domain/product/core/dto/product_dto"
+	"getfund-api-v2/pkg/db/schema"
+
+	"gorm.io/gorm"
+)
+
+type productRepository struct {
+	db *gorm.DB
+}
+
+func New(db *gorm.DB) product_contract.Repository {
+	return &productRepository{db: db}
+}
+
+func (p *productRepository) GetProductById(productId int) (*product_dto.ProductDto, error) {
+	var product = &schema.Product{}
+
+	result := p.db.
+		Select("id, is_active").
+		Where("id=?", productId).
+		First(product)
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return nil, nil
+}
